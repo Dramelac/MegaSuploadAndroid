54d39dab86eaf1c3c63a2a6a69a91f588b7f7a1c
Dorian <208108@supinfo.com>
2018-04-30 14:27:08 +0200

    Add login store in Sharedprefernces and add session

--- app/src/main/java/com/megasupload/megasuploadandroidapp/HomePage.java
+++ app/src/main/java/com/megasupload/megasuploadandroidapp/HomePage.java
@@ -1,13 +1,55 @@
 package com.megasupload.megasuploadandroidapp;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class HomePage extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.megasupload.megasuploadandroidapp.UserSession.KEY_NAME;
+import static com.megasupload.megasuploadandroidapp.UserSession.PREFER_NAME;
+
+public class HomePage extends Activity {
+
+
+    @BindView(R.id.welcomeTextView)
+    TextView welcomeTextView;
+
+    @BindView(R.id.logoutButton)
+    Button logoutButton;
+
+
+    private SharedPreferences sharedPreferences;
+
+    UserSession session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        session = new UserSession(getApplicationContext());
+        ButterKnife.bind(this);
+
+        sharedPreferences = getApplicationContext().getSharedPreferences(PREFER_NAME, MODE_PRIVATE);
+        String name = sharedPreferences.getString(KEY_NAME, null);
+        welcomeTextView.setText("Hello welcome " + name);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                session.logoutUser();
+            }
+        });
+
     }
 }
+
---
+++ app/src/main/java/com/megasupload/megasuploadandroidapp/LoginActivity.java
@@ -0,0 +1,149 @@
+package com.megasupload.megasuploadandroidapp;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends Activity {
+
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+    private static final String PREFER_NAME = "Reg";
+
+
+
+    @BindView(R.id.loginEditText)
+    EditText loginEditText;
+
+    @BindView(R.id.passwordEditText)
+    EditText passwordEditText;
+
+    @BindView(R.id.loginButton)
+    Button loginButton;
+
+    int counter = 3;
+
+    UserSession session;
+
+    private SharedPreferences sharedPreferences;
+
+    private boolean ok = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
+        final Intent intent = new Intent(this, HomePage.class);
+        session = new UserSession(getApplicationContext());
+        Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(),
+                Toast.LENGTH_LONG).show();
+
+        if (session.isLoggedIn()) {
+            startActivity(intent);
+            finish();
+        }
+
+        sharedPreferences = this.getSharedPreferences(PREFER_NAME, Context.MODE_PRIVATE);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login(intent);
+            }
+        });
+
+
+        }
+    public void login (final Intent intent) {
+        Log.d(TAG, "Login");
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.Theme_AppCompat_DayNight_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        final String userName = loginEditText.getText().toString();
+        session.createUserLoginSession(userName);
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        onLoginSuccess();
+                        startActivity(intent);
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the LoginActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+
+        loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String userName = loginEditText.getText().toString();
+
+        if (userName.isEmpty()) {
+            loginEditText.setError("enter an UserName");
+            valid = false;
+        } else {
+            loginEditText.setError(null);
+        }
+
+        return valid;
+    }
+}
+
