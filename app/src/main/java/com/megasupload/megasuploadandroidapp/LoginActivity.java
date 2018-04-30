bc598a716377aeb8e59aa66af75bbd184050f3c1
Dorian <208108@supinfo.com>
2018-04-30 17:05:42 +0200

    Test post login api

--- app/src/main/java/com/megasupload/megasuploadandroidapp/LoginActivity.java
+++ app/src/main/java/com/megasupload/megasuploadandroidapp/LoginActivity.java
@@ -5,18 +5,29 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -84,8 +95,8 @@ public class LoginActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                AsyncT asyncT = new AsyncT();
-                asyncT.execute();
+                new HttpAsyncTask().execute("https://megasupload.lsd-music.fr/api/auth/login");
+
             }
         });
 
@@ -163,6 +174,92 @@ public class LoginActivity extends Activity {
         }
 
         return valid;
+    }
+    public static String POST(String url, User user){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+
+            String json = "";
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("login", user.getLogin());
+            jsonObject.accumulate("password", user.getPassword());
+
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            httpPost.setHeader("X-CSRFToken","G2IMbiAntBExBAzMHhXdhU4EAEK9rEmizhOQWeTv8j7lAYaGuVdp3tkztHKzEJjb");
+            httpPost.setHeader("cookie","csrftoken=G2IMbiAntBExBAzMHhXdhU4EAEK9rEmizhOQWeTv8j7lAYaGuVdp3tkztHKzEJjb");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // 10. convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            User user = new User();
+            user.setLogin("dorian");
+            user.setPassword("SupinfO!");
+
+
+            return POST(urls[0],user);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+        }
+    }
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 }
 
