c38f5fcd82dc709c83ef2a7a18079135a5bb94c1
Nico <204343@supinfo.com>
2018-05-23 17:05:35 +0200

    found file fait

---
+++ app/src/main/java/com/megasupload/megasuploadandroidapp/Files.java
@@ -0,0 +1,43 @@
+package com.megasupload.megasuploadandroidapp;
+
+public class Files implements Comparable<Files> {
+    private String name;
+    private String data;
+    private String date;
+    private String path;
+    private String image;
+
+    public Files(String n,String d, String dt, String p, String img)
+    {
+        name = n;
+        data = d;
+        date = dt;
+        path = p;
+        image = img;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getData()
+    {
+        return data;
+    }
+    public String getDate()
+    {
+        return date;
+    }
+    public String getPath()
+    {
+        return path;
+    }
+    public String getImage() {
+        return image;
+    }
+    public int compareTo(Files o) {
+        if(this.name != null)
+            return this.name.toLowerCase().compareTo(o.getName().toLowerCase());
+        else
+            throw new IllegalArgumentException();
+    }
+}
